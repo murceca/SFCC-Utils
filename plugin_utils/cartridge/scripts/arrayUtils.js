a7399c76a987bcb9308a82d0f46423e3ff316575
Svitlana Hryshchuk <r.svetulik@gmail.com>
2023-11-18 22:24:50 +0100

    Improved 'unique' function - addeded support of callback processor

--- plugin_utils/cartridge/scripts/arrayUtils.js
+++ plugin_utils/cartridge/scripts/arrayUtils.js
@@ -1,19 +1,31 @@
 /**
  * Returns an array of unique values based on the provided array.
- *
- * @param {array} - The source array.
- * @returns {array} - The array of unique values.
- *
+ * If a callback function is provided, it is applied to each element 
+ * of the array to determine uniqueness based on the transformed values.
+ * 
+ * @param {array} - The input array.
+ * @param {function} [callback] - A callback function applied to each element
+ * of the array. It should return a value used for uniqueness comparison.
+ * @returns {array} - An array containing unique values based 
+ * on the provided array.
+ * 
  * @example
  * unique(['DE', 'BE', 'DE', 'CZ','NL', 'DK', 'NL', 'EE']);
  * // [ 'DE', 'BE', 'CZ', 'NL', 'DK', 'EE' ]
+ * 
+ * unique(['DE', 'BE', 'DE', 'CZ','NL', 'DK', 'NL', 'EE'], item => item[0]);
+ * // [ 'DE', 'BE', 'CZ', 'NL', 'EE' ]
  */
-const unique = (array) => {
+const unique = (array, callback) => {
   const uniqueValues = [];
-  array.forEach(value => {
-    const isUnique = !uniqueValues.includes(value);
+  array.forEach(item => {
+    const value = callback ? callback(item) : item;
+    const isUnique = !uniqueValues.find(uniqueItem => {
+      const returnedValue = callback ? callback(uniqueItem) : uniqueItem;
+      return returnedValue === value;
+    });
     if (isUnique) {
-      uniqueValues.push(value);
+      uniqueValues.push(item);
     }
   });
   return uniqueValues;

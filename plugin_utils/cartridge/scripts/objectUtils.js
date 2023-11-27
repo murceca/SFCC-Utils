b76143620c917849dc4f7eb35e9c32abc24e9cba
Svitlana Hryshchuk <r.svetulik@gmail.com>
2023-11-27 03:55:32 +0100

    Added 'pick' function

--- plugin_utils/cartridge/scripts/objectUtils.js
+++ plugin_utils/cartridge/scripts/objectUtils.js
@@ -59,7 +59,51 @@ const get = (obj, path) => {
   }
 }
 
+/**
+ * Creates a new object by picking specific properties from a source object
+ * based on either a list of property names or a filtering function.
+ * 
+ * @param {object} primaryObject - The source object from which 
+ * properties will be picked.
+ * @param {...(string|function)} args - Either an array of property 
+ * names to pick, or a filtering function to determine inclusion.
+ * @returns {object} - A new object containing only the selected properties
+ * from the source object.
+ * 
+ * @example
+ * Picking specific properties by name
+ * pick({ id: 1, name: 'productName', size: 500 }, 'id', 'name');
+ * { id: 1, name: 'productName'}
+ * 
+ * Picking properties based on a filtering function
+ * pick({ id: 1, name: 'productName', size: 500 }, (key, value) => value >= 500);
+ * { size: 500 }
+ */
+const pick = function() {
+  const newObject = {};
+  const primaryObject = arguments[0];
+  const args = Array.prototype.slice.call(arguments);
+  const argsToPick = args.slice(1);
+  if (typeof(args[1]) !== 'function') {
+      argsToPick.forEach(key => {
+      newObject[key] = primaryObject[key];
+    });
+  } else {
+    const filterFn = args[1];
+    Object.keys(primaryObject).forEach(key => {
+      const val = primaryObject[key];
+      const toInclude = filterFn(key, val);
+      if (toInclude) {
+        newObject[key] = primaryObject[key];
+      }
+    });
+  }
+  
+  return newObject;
+}
+
 module.exports = {
   parseJSON,
-  get
+  get,
+  pick
 };

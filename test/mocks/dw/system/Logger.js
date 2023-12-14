6e2f69db7f911899db02a6e9be58ab6eba112250
Svitlana Hryshchuk <r.svetulik@gmail.com>
2023-12-14 18:47:47 +0100

    Created path for Logger and added it to code accordingly

---
+++ test/mocks/dw/system/Logger.js
@@ -0,0 +1,4 @@
+module.exports = {
+  info: () => {},
+  error: () => {},
+};
--- test/plugin_utils/cartridge/scripts/objectUtils.spec.js
+++ test/plugin_utils/cartridge/scripts/objectUtils.spec.js
@@ -1,42 +1,35 @@
 const { expect } = require('chai');
 const proxyquire = require('proxyquire');
 
-const mockLogger = {
-  error: () => {}
-};
-
-const objectUtilsWithMockLogger = proxyquire.noCallThru().load('../../../../plugin_utils/cartridge/scripts/objectUtils', {
-  'dw/system/Logger': mockLogger,
+const objectUtils = proxyquire.noCallThru().load('../../../../plugin_utils/cartridge/scripts/objectUtils', {
+  'dw/system/Logger': require('../../../mocks/dw/system/Logger'),
 });
 
 describe('parseJSON function', () => {
+  const { parseJSON } = objectUtils;
+
   it('parse a simple string', () => {
-    const { parseJSON } = objectUtilsWithMockLogger;
     const actualResult = parseJSON('{"countryCode":"CZ"}');
     expect(actualResult).to.deep.eql({ "countryCode": "CZ" });
   });
 
   it('an empty string', () => {
-    const { parseJSON } = objectUtilsWithMockLogger;
     const actualResult = parseJSON('');
     expect(actualResult).to.be.null;
   });
 
   it('invalid JSON', () => {
-    const { parseJSON } = objectUtilsWithMockLogger;
     const jsonString = 'invalid-json';
     const actualResult = parseJSON(jsonString);
     expect(actualResult).to.be.null;
   });
 
   it('null input', () => {
-    const { parseJSON } = objectUtilsWithMockLogger;
     const actualResult = parseJSON(null);
     expect(actualResult).to.be.null;
   });
 
   it('undefined input', () => {
-    const { parseJSON } = objectUtilsWithMockLogger;
     const actualResult = parseJSON(undefined);
     expect(actualResult).to.be.null;
   });
